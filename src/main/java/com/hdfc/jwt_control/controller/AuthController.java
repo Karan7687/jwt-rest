b94308278973498355f2146ca4718416bb8a76d2
Karan Kamble <KARAN.KAMBLE@hdfclife.com>
2026-09-22 20:27:01 +0530

    feat(auth): add user registration API using Service Layer

--- src/main/java/com/hdfc/jwt_control/controller/AuthController.java
+++ src/main/java/com/hdfc/jwt_control/controller/AuthController.java
@@ -1,20 +1,27 @@
 package com.hdfc.jwt_control.controller;
 
 import com.hdfc.jwt_control.model.RegisterRequest;
+import com.hdfc.jwt_control.service.AuthService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
+    private final AuthService authService;
 
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+    //"Take the service object provided to me and store it inside my controller."
 
-        @PostMapping("/register")
-        public String register(@RequestBody RegisterRequest request){
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
 
-                return "Registration request received for " + request.getUsername();
+        return ResponseEntity
+                .status(201)
+                .body(authService.register(request.getUsername()));
 
-        }
-
-
+    }
 }
